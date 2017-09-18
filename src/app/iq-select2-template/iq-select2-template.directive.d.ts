0ea7df717700ab118c0a03b97ba7e93da00fbe30
Vivien Achet <achetv84@gmail.com>
2017-09-18 22:32:34 +0200

    commit

---
+++ src/app/iq-select2-template/iq-select2-template.directive.d.ts
@@ -0,0 +1,6 @@
+import { TemplateRef } from '@angular/core';
+import { IqSelect2Component } from '../iq-select2/iq-select2.component';
+export declare class IqSelect2TemplateDirective {
+    private templateRef;
+    constructor(templateRef: TemplateRef<any>, host: IqSelect2Component);
+}
